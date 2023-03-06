e27b78c30648ec7e0be788613a48b9e138131057
shubham <shubhamkhalse12@gmail.com>
2023-03-06 05:13:16 -0700

    Module Pushed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Department', loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
