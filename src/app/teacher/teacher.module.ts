1d4fef55c0afa7554522830e19037e8f61bb1d9f
11vishakha <vishakhamhasaye04@gmail.com>
2023-03-10 11:34:49 +0530

    updated

---
+++ src/app/teacher/teacher.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeacherRoutingModule } from './teacher-routing.module';
+import { TeacherComponent } from './teacher.component';
+import { TeacherListComponent } from './teacher-list/teacher-list.component';
+import { TeacherAddComponent } from './teacher-add/teacher-add.component';
+import { TeacherViewComponent } from './teacher-view/teacher-view.component';
+import { TeacherEditComponent } from './teacher-edit/teacher-edit.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+@NgModule({
+  declarations: [
+    TeacherComponent,
+    TeacherListComponent,
+    TeacherAddComponent,
+    TeacherViewComponent,
+    TeacherEditComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    TeacherRoutingModule,
+    FontAwesomeModule
+  ]
+})
+export class TeacherModule { }
