c82b73f1b0c5374d0f54206c1c62824f23797fbb
krishe1516 <sheetalshinde855@gmail.com>
2023-03-13 22:55:13 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { HolidayComponent } from './holiday/holiday.component';
 
 
 const routes: Routes = [
+  {path:'student', loadChildren: ()=>import('./student/student.module').then(m=>m.StudentModule)},
   { path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) },
-{ path: 'holiday', loadChildren: () => import('./holiday/holiday.module').then(m => m.HolidayModule) }];
+{ path: 'holiday', loadChildren: () => import('./holiday/holiday.module').then(m => m.HolidayModule) },
+{path:'events' ,loadChildren:()=> import('./event/event.module').then(m=>m.EventModule)},
+  { path: 'exams', loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) }];
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentRoutingModule } from './student/student-routing.module';
+import { TeacherRoutingModule } from './teacher/teacher-routing.module';
+import { HolidayRoutingModule } from './holiday/holiday-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EventRoutingModule } from './event/event-routing.module';
+import { ExamsRoutingModule } from './exams/exams-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-  
+    
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StudentRoutingModule,
+    TeacherRoutingModule,
+    HolidayRoutingModule,
+    FontAwesomeModule,
+    EventRoutingModule,
+    ExamsRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
