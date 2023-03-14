3a6fe1005159d402d94cfcc5d055295b4cb9065e
Abhishek8177 <abhishekwankhede326@gmail.com>
2023-03-14 14:34:03 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LibraryComponent } from './library/library.component';
 import { TableComponent } from './table/table.component';
 
@@ -22,16 +21,6 @@ const routes: Routes = [
     path:'library', component:LibraryComponent
   }
 ];
-=======
-import { HolidayComponent } from './holiday/holiday.component';
-
-
-const routes: Routes = [
-  { path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) },
-{ path: 'holiday', loadChildren: () => import('./holiday/holiday.module').then(m => m.HolidayModule) }];
-
-
->>>>>>> 9fc305344a438270247ca7a1d27e6f633ad7d09f
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
